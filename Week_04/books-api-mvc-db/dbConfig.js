17c6d8ffe3828d376ade39c31904d0ee5d053cd2
Shin Thant Aung <150310252+Shin-ThantAung@users.noreply.github.com>
2024-06-28 12:22:07 +0800

    Finish week 6

--- Week_04/books-api-mvc-db/dbConfig.js
+++ Week_04/books-api-mvc-db/dbConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
     user: "booksapi_shin", // Replace with your SQL Server login username
-    password: "booksapi_$hinTA", // Replace with your SQL Server login password
+    password: "BEDdbShin", // Replace with your SQL Server login password
     server: "localhost",
     database: "bed_db",
     trustServerCertificate: true,
